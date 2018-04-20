42a5b3ce2d54f5fed3ea2a0437a220293e83f137
bizzahbatool <37095415+bizzahbatool@users.noreply.github.com>
2018-04-20 16:55:19 +0500

    bizzah

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,18 @@ var router = express.Router();
 // 	res.render('./layouts/SignInSignUp');
 // });
 router.get('/', ensureAuthenticated, function(req, res){
-	res.render('./layouts/TEMPLATE');
+<<<<<<< HEAD
+	res.render('./layouts/SignInSignUp');
+=======
+	if (req.user.firstName == "")
+		res.render('./layouts/Edit_Profile_Student')
+	else{
+		if(req.user.type == "Instructor")
+			res.render('./layouts/TEMPLATE');
+		else if (req.user.type == "Student")
+			res.render('./layouts/StudentMain');
+	}
+>>>>>>> 7d1c5e96fbf6ec5969c279a0c09333061978c06a
 });
 
 function ensureAuthenticated(req, res, next){
@@ -18,4 +29,28 @@ function ensureAuthenticated(req, res, next){
 	}
 }
 
+router.get('/profile', ensureAuthenticated, function(req,res){
+	res.render('./layouts/ProfileInst');
+})
+
+router.get('/courses', ensureAuthenticated, function(req,res){
+	if(req.user.type == "Instructor")
+			res.render('./layouts/Resources_Instructor');
+	else if (req.user.type == "Student")
+		res.render('./layouts/Resources_Student');
+})
+
+router.get('/assignments', ensureAuthenticated, function(req,res){
+	if(req.user.type == "Instructor")
+			res.render('./layouts/Assignment_Instructor');
+	else if (req.user.type == "Student")
+		res.render('./layouts/Assignment_Student');
+})
+
+router.get('/addAssignment', ensureAuthenticated, function(req,res){
+	res.render('./layouts/AddAssignDetails');
+})
+router.get('/addStudent', ensureAuthenticated, function(req,res){
+	res.render('./layouts/AddStudent');
+})
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,29 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://saadn22:Saad7223@ds131137.mlab.com:31137/tapro');
+var db = mongoose.connection;
+
+var multer = require('multer')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+});
+
+var upload = multer({ storage: storage }).any();
 
 var User = require('../models/user');
+
+router.post('/addAssignment', upload, (req, res, next) => {
+  console.log(req.files)
+  res.redirect('/')
+});
 
 // Register
 router.get('/register', function(req, res){
@@ -25,21 +46,17 @@ router.post('/register', function(req, res){
 	var password2 = req.body.password2;
 	var ContactNumber = req.body.ContactNumber;
 
-	console.log(req.body)
-	if (password == password2)
-		console.log("FIT");
-	else
-		console.log(typeof password2);
-
+	// console.log(req.body)
 
 	// Validation
 	req.checkBody('firstName', 'First Name is required').notEmpty();
 	req.checkBody('lastName', 'Last Name is required').notEmpty();
 	req.checkBody('email', 'Email is required').notEmpty();
 	req.checkBody('email', 'Email is not valid').isEmail();
-	req.checkBody('ContactNumber', 'Email is not valid').notEmpty();
+	req.checkBody('ContactNumber', 'Contact Number is required').notEmpty();
 	req.checkBody('password', 'Password is required').notEmpty();
 	req.checkBody('password', 'Passwords do not match').equals(req.body.password2);
+
 
 	var errors = req.validationErrors();
 	console.log(errors)
@@ -48,22 +65,151 @@ router.post('/register', function(req, res){
 			errors:errors
 		});
 	} else {
-		var newUser = new User({
-			firstName: firstName,
-			lastName: lastName,
-			email:email,
-			password: password,
-			ContactNumber: ContactNumber,
+
+	// 	User.getUserByEmail(email, function(err, user){
+	// 	   	if(err) {
+	// 	   		console.log(err)
+	// 	   	};
+	// 	   	if(user){
+	// 	   		console.log("DUPLICATE");
+	// req.flash('success_msg', 'You are logged out');
+	// 	   		// req.flash('error_msg', 'Email already exists');
+	// 	   		res.redirect('/users/login');
+	// 	   	}
+	//    	});
+		console.log(email);
+		var emailCheck = false;
+		db.collection('users').findOne({email: email}, function(err, doc) {
+		    if (err) {
+		      console.log(err)
+		    } else {
+		    	if(doc) {
+		    		req.flash('error_msg', 'Email already exists');
+		    		console.log("REDIRECT DUPLICATE")
+					res.redirect('/users/login');
+		      	}
+		      	else{
+		      		var newUser = new User({
+						firstName: firstName,
+						lastName: lastName,
+						email:email,
+						password: password,
+						ContactNumber: ContactNumber,
+						type: 'Instructor',
+						ParentName: null,
+						ParentContact: null
+					});
+
+					User.createUser(newUser, function(err, user){
+						if(err) throw err;
+						console.log(user);
+					});
+
+					req.flash('success_msg', 'You are registered and can now login');
+					console.log('flash message')
+					res.redirect('/users/login');
+		      	}
+		    }
+		});
+	}
+});
+
+router.post('/registerStudent', function(req, res){
+	var firstName = req.body.firstName;
+	var lastName = req.body.lastName;
+	// var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+	var ContactNumber = req.body.ContactNumber;
+	var ParentContactNumber = req.body.ParentContactNumber;
+	var pname = req.body.pname;
+
+
+	// console.log(req.body)
+
+	// Validation
+	req.checkBody('firstName', 'First Name is required').notEmpty();
+	req.checkBody('pname', 'Parent Name is required').notEmpty();
+	req.checkBody('lastName', 'Last Name is required').notEmpty();
+	req.checkBody('ContactNumber', 'Contact Number is required').notEmpty();
+	req.checkBody('ParentContactNumber', 'Parent Contact Number is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password', 'Passwords do not match').equals(req.body.password2);
+
+
+	var errors = req.validationErrors();
+	console.log(errors)
+	if(errors){
+		res.render('./layouts/Edit_Profile_Student',{
+			errors:errors
+		});
+	} else {
+		let user = req.user;
+		console.log(user)
+		user.firstName = firstName;
+		user.lastName = lastName;
+		user.pname = pname;
+		user.ContactNumber = ContactNumber;
+		user.ParentContactNumber = ParentContactNumber;
+
+		db.collection('users').updateOne({ 'email': user.email }, {$set : user}, function(err, docy) {
+          if (err) {
+             console.log(err);
+           }
+           res.redirect('/');
+        });
+	}
+});
+
+router.post('/addStudent', function(req, res){
+	var email = req.body.email;
+	var password = req.body.password;
+
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('password', 'Password is required').notEmpty();
+	var errors = req.validationErrors();
+	console.log(errors)
+	if(errors){
+		res.render('./layouts/AddStudent',{
+			errors:errors
+		});
+	} else {
+
+		var emailCheck = false;
+		db.collection('users').findOne({email: email}, function(err, doc) {
+		    if (err) {
+		      console.log(err)
+		    } else {
+		    	if(doc) {
+		    		console.log("ERROR")
+		    		req.flash('error_msg', 'Email already exists');
+					res.redirect('/addStudent');
+		      	}
+		      	else{
+		      		var newUser = new User({
+						firstName: "",
+						lastName: "",
+						email:email,
+						password: password,
+						ContactNumber: "",
+						type: 'Student',
+						ParentName: "",
+						ParentContact: ""
+					});
+
+					User.createUser(newUser, function(err, user){
+						if(err) throw err;
+						console.log(user);
+					});
+					console.log("ADDED ")
+					req.flash('success_msg', 'Added User');
+					console.log('flash message')
+					res.redirect('/addStudent');
+		      	}
+		    }
 		});
 
-		User.createUser(newUser, function(err, user){
-			if(err) throw err;
-			console.log(user);
-		});
-
-		req.flash('success_msg', 'You are registered and can now login');
-		console.log('flash message')
-		res.redirect('/');
 	}
 });
 
@@ -74,7 +220,6 @@ passport.use(new LocalStrategy( {usernameField: 'email',
 
    User.getUserByEmail(email, function(err, user){
    	if(err) {
-   		console.log(err);
    		throw err
    	};
    	if(!user){
